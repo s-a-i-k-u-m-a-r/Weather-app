ee46e7d912aeba4d8ee5e7091fb370dd9548dfed
s-a-i-k-u-m-a-r <88674918+s-a-i-k-u-m-a-r@users.noreply.github.com>
2023-07-22 16:50:52 +0530

    script.js

--- script.js
+++ script.js
@@ -1,145 +1,167 @@
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "fde930d237mshe5207ed7cf5aa29p1f0ef4jsn19fa5f2e8015",
-    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
-  },
-};
-function convertUnixTimestampToTime(unixTimestamp) {
-  const date = new Date(unixTimestamp * 1000);
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const timeString = `${hours.toString().padStart(2, "0")}:${minutes
-    .toString()
-    .padStart(2, "0")}`;
-  return timeString;
-}
-const getWeather = (city) => {
-  cityName.innerHTML = city;
-  fetch(
-    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response);
-      cloud_pct.innerHTML = response.cloud_pct;
-      temp.innerHTML = response.temp;
-      temp2.innerHTML = response.temp;
-      feels_like.innerHTML = response.feels_like;
-      humidity.innerHTML = response.humidity;
-      humidity2.innerHTML = response.humidity;
-      min_temp.innerHTML = response.min_temp;
-      max_temp.innerHTML = response.max_temp;
-      wind_speed.innerHTML = response.wind_speed;
-      wind_speed2.innerHTML = response.wind_speed;
-      wind_degrees.innerHTML = response.wind_degrees;
-      sunrise.innerHTML = convertUnixTimestampToTime(response.sunrise);
-      sunset.innerHTML = convertUnixTimestampToTime(convertUnixTimestampToTime(response.sunset));
-      if(city=="delhi"){
-        cityName.innerHTML = city
-        Cloud_pct.innerHTML = response.cloud_pct
-        temp.innerHTML = response.temp
-        temp2.innerHTML = response.temp
-        Humidity.innerHTML = response.humidity
-        Humidity2.innerHTML = response.humidity
-        min_temp.innerHTML = response.min_temp
-        max_temp.innerHTML = response.max_temp
-        Wind_speed.innerHTML = response.wind_speed
-        Wind_speed2.innerHTML = response.wind_speed
-        Wind_degrees.innerHTML = response.wind_degrees
-        Sunrise.innerHTML = convertUnixTimestampToTime(response.sunrise)
-        Sunset.innerHTML = convertUnixTimestampToTime(convertUnixTimestampToTime(response.sunset))
-    }
+.form-control {
+    border: 2px solid #6c757d;
+    border-radius: 10px;
+    font-size: 16px;
+    padding: 8px 12px;
+    transition: border-color 0.2s ease-in-out;
+  }
   
-    else if(city=="pune"){
-    Cloud_pctpune.innerHTML = response.cloud_pct
-    temppune.innerHTML = response.temp
-    Humiditypune.innerHTML = response.humidity
-    min_temppune.innerHTML = response.min_temp
-    max_temppune.innerHTML = response.max_temp
-    Wind_speedpune.innerHTML = response.wind_speed
-    Wind_degreespune.innerHTML = response.wind_degrees
-    Sunrisepune.innerHTML = convertUnixTimestampToTime(response.sunrise)
-    Sunsetpune.innerHTML = convertUnixTimestampToTime(convertUnixTimestampToTime(response.sunset))
+  .form-control:focus {
+    outline: none;
+    border-color: #040404;
+    box-shadow: 0 rgb(19, 198, 52)rem rgba(0, 123, 255, 0.25);
+  }
+  
+ 
+
+  .btn-outline-success {
+    border-color: #0d0f0e;
+    color: #000000;
+    transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+    cursor: pointer; 
+    border-radius: 10px;
+  }
+  
+  .btn-outline-success:hover {
+    background-color: #010302;
+    color: #ffffff;
+  }
+  
+  .navbar-brand {
+    font-size: 24px;
+    font-weight: bold;
+  }
+ 
+  
+  
+  /* Style for the dropdown menu */
+  .dropdown-menu {
+    padding: 10px;
+    border-radius: 4px;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+    min-width: 250px;
+    line-height: 1.6;
+    font-size: 14px;
+  }
+  
+  .dropdown-item {
+    color: #333;
+    padding: 5px 10px;
+    transition: background-color 0.2s ease-in-out;
+  }
+  
+  .dropdown-item:hover {
+    background-color: #f8f9fa;
+    color: #333;
+  }
+  
+  #websiteInfo {
+    background-color: #c7c40f;
+    color: #000000;
+    border-radius: 5px;
+    padding: 10px;
+    transition: background-color 0.3s ease-in-out;
+  }
+  
+  #websiteInfo:hover {
+    background-color: #9da0a3;
+    animation: bounce 0.3s linear;
+  }
+  
+  @keyframes bounce {
+    0% {
+      transform: translateY(0);
     }
-    else if(city=="mumbai")
-    {
-    Cloud_pctmumbai.innerHTML = response.cloud_pct
-    tempmumbai.innerHTML = response.temp
-    Humiditymumbai.innerHTML = response.humidity
-    min_tempmumbai.innerHTML = response.min_temp
-    max_tempmumbai.innerHTML = response.max_temp
-    Wind_speedmumbai.innerHTML = response.wind_speed
-    Wind_degreesmumbai.innerHTML = response.wind_degrees
-    Sunrisemumbai.innerHTML = convertUnixTimestampToTime(response.sunrise)
-    Sunsetmumbai.innerHTML = convertUnixTimestampToTime(response.sunset)
+    50% {
+      transform: translateY(-3px);
     }
-    else if(city=="hyderabad")
-    {
-    Cloud_pcthyderbad.innerHTML = response.cloud_pct
-    temphyderbad.innerHTML = response.temp
-    Humidityhyderbad.innerHTML = response.humidity
-    min_temphyderbad.innerHTML = response.min_temp
-    max_temphyderbad.innerHTML = response.max_temp
-    Wind_speedhyderbad.innerHTML = response.wind_speed
-    Wind_degreeshyderbad.innerHTML = response.wind_degrees
-    Sunrisehyderbad.innerHTML = convertUnixTimestampToTime(response.sunrise)
-    Sunsethyderbad.innerHTML = convertUnixTimestampToTime(response.sunset)
+    100% {
+      transform: translateY(0);
     }
-    else if(city=="chennai")
-    {
-    Cloud_pctchennai.innerHTML = response.cloud_pct
-    tempchennai.innerHTML = response.temp
-    Humiditychennai.innerHTML = response.humidity
-    min_tempchennai.innerHTML = response.min_temp
-    max_tempchennai.innerHTML = response.max_temp
-    Wind_speedchennai.innerHTML = response.wind_speed
-    Wind_degreeschennai.innerHTML = response.wind_degrees
-    Sunrisechennai.innerHTML = convertUnixTimestampToTime(response.sunrise)
-    Sunsetchennai.innerHTML = convertUnixTimestampToTime(response.sunset)
-    } 
-    })
-    .catch((err) => console.error(err));
-};
+  }
+  .toggle-container {
+    display: flex;
+    align-items: center;
+    justify-content: right;
+    margin-top: 20px;
+  margin-right: 110px;
+  }
+  
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 40px;
+    height: 20px;
+  }
+  
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+  
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #000000;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+    border-radius: 34px;
+  }
+  
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 16px;
+    width: 16px;
+    left: 2px;
+    bottom: 2px;
+    background-color: rgb(188, 188, 188);
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+    border-radius: 50%;
+  }
+  
+  input:checked + .slider {
+    background-color: #efeff2;
+  }
+  
+  input:checked + .slider:before {
+    -webkit-transform: translateX(20px);
+    -ms-transform: translateX(20px);
+    transform: translateX(20px);
+  }
+  
+  .slider.round {
+    border-radius: 34px;
+  }
+  
+  .slider.round:before {
+    border-radius: 50%;
+  }
+  body.dark-mode {
+    background-color: #333;
+    color: #fff;
+  }
 
-submit.addEventListener("click", (e) => {
-  e.preventDefault();
-  getWeather(city.value);
-});
+  .custom-container {
+    background-color: #9bb0b3; /* Replace with your desired background color */
+    border-radius: 10px; /* Add rounded corners (adjust the value as needed) */
+    padding: 20px; /* Add some padding to give it some space */
+    box-shadow: 0 8px 10px rgba(151, 28, 28, 0.1); /* Add a subtle box shadow */
+  }
 
-getWeather("Delhi");
-getWeather("pune")
-getWeather("mumbai")
-getWeather("hyderabad")
-getWeather("chennai")
-
-const darkModeToggle = document.getElementById("darkModeToggle");
-const body = document.body;
-
-darkModeToggle.addEventListener("change", () => {
-  body.classList.toggle("dark-mode");
-  const toggleText = document.getElementById("toggle-text");
-  toggleText.textContent = body.classList.contains("dark-mode")
-    ? "Light Mode"
-    : "Dark Mode";
-});
-
-// Check if dark mode is already enabled from previous session
-if (localStorage.getItem("darkModeEnabled")) {
-  body.classList.add("dark-mode");
-  darkModeToggle.checked = true;
+.container-fluid.custom-container {
+  
+  margin: 0 auto;
+  max-width: 1400px; /* Adjust the maximum width as needed */
 }
 
-//   if (city == "Newyork") {
-    //     cloud_pct_ny.innerHTML = response.cloud_pct;
-    //     temp_ny.innerHTML = response.temp;
-    //     feels_like_ny.innerHTML = response.feels_like;
-    //     humidity_ny.innerHTML = response.humidity;
-    //     min_temp_ny.innerHTML = response.min_temp;
-    //     max_temp_ny.innerHTML = response.max_temp;
-    //     wind_speed_ny.innerHTML = response.wind_speed;
-    //     wind_degrees_ny.innerHTML = response.wind_degrees;
-    //     sunrise_ny.innerHTML = convertUnixTimestampToTime(response.sunrise);
-    //     sunset_ny.innerHTML = convertUnixTimestampToTime(convertUnixTimestampToTime(response.sunset));
-    //   }
+.text-bg{
+  background-color: gray;
+}
+  
